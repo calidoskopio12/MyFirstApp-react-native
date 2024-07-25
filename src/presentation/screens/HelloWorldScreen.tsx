b6ce6bfab036c568f9ee64c7cbbfa4b4a52de724
unknown <maximiliano.sandalie@gmail.com>
2024-07-25 12:16:00 -0300

    first commit

---
+++ src/presentation/screens/HelloWorldScreen.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export const HelloWorldScreen = () => {
+  return (
+   <View style= {styles.container}> 
+    <Text style={styles.title}>
+        Hello World
+    </Text>
+   </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    
+    },
+    title: {
+        fontSize: 45,
+        textAlign: 'center',
+        color: 'black',
+        padding: 20
+    } 
+})
